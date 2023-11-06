6785855dc1b294e5c60b2c28a1b0e5d0e0ff08be
Kambar-ZH <zhamauov02@mail.ru>
2023-11-06 19:31:44 +0600

    refactor seg tree single update

--- templates/segment-tree/single-update.cpp
+++ templates/segment-tree/single-update.cpp
@@ -46,104 +46,121 @@ void read2(vt<vt<T> > & a) {
 	For(i, a.size()) For(j, a[i].size()) cin >> a[i][j];
 }
 
-enum UPD_POL { SUM, MIN, MAX };
- 
-template <UPD_POL upd_pol>
-struct seg_tree {
-	int n;
-	vt<int> a, tree;
-	seg_tree(vt<int> _a) {
-		n = _a.size(), a = _a;
-		tree.resize(4 * n);
+struct node {
+	ll value;
+	node() {}
+	node(ll value) : value(value) {};
+
+	static node merge(node & n1, node & n2) {
+		node r(n1.value + n2.value);
+		return r;
 	}
 
-	int sum(int a, int b) {
-		return a + b;
+	static node identitity() {
+		node r(0);
+		return r;
 	}
- 
+};
+
+template<typename T>
+struct seg_tree_single {
+	int n;
+	vt<T> a, tree;
+	seg_tree_single(vt<T> a) {
+		this->n = a.size(); 
+		this->a = a;
+		this->tree.resize(4 * n);
+		this->build();
+	}
+
+	seg_tree_single(int n) {
+		this->n = n; 
+		this->tree.resize(4 * n, T::identitity());
+	}
+
 	void _build(int x, int l, int r) {
 		if (l == r) {
 			tree[x] = a[l];
 			return;
 		}
+
 		int m = (l + r) >> 1;
+
 		_build(ls(x), l, m);
 		_build(rs(x), m + 1, r);
-		if constexpr (upd_pol == MIN) {
-            tree[x] = min(tree[ls(x)], tree[rs(x)]);
-        } else if constexpr (upd_pol == MAX) {
-            tree[x] = max(tree[ls(x)], tree[rs(x)]);
-		} else {
-            tree[x] = sum(tree[ls(x)], tree[rs(x)]);
-		}
+		
+		tree[x] = T::merge(tree[ls(x)], tree[rs(x)]);
 	}
  
 	void build() {
 		_build(0, 0, n - 1);
 	}
  
-	int _get_query(int x, int lx, int rx, int l, int r) {
+	T _get(int x, int lx, int rx, int l, int r) {
 		if (r < lx || rx < l) {
-			if constexpr (upd_pol == MIN) {
-            	return INT_MAX;
-			} else if constexpr (upd_pol == MAX) {
-				return -INT_MAX;
-			} else {
-				return 0;
-			}
+			return T::identitity();
 		}
+		
 		if (l <= lx && rx <= r) {
 			return tree[x];
 		}
+
 		int mx = (lx + rx) >> 1;
-		int left_subtree = _get_query(ls(x), lx, mx, l, r);
-		int right_subtree = _get_query(rs(x), mx + 1, rx, l, r);
-		if constexpr (upd_pol == MIN) {
-            return min(left_subtree, right_subtree);
-        } else if constexpr (upd_pol == MAX) {
-            return max(left_subtree, right_subtree);
-		} else {
-            return sum(left_subtree, right_subtree);
-		}
+		
+		T left_subtree = _get(ls(x), lx, mx, l, r);
+		T right_subtree = _get(rs(x), mx + 1, rx, l, r);
+
+		return T::merge(left_subtree, right_subtree);
 	}
  
-	int get_query(int l, int r) {
-		return _get_query(0, 0, n - 1, l, r);
+	T get(int l, int r) {
+		return _get(0, 0, n - 1, l, r);
 	}
 
-	void _update(int x, int lx, int rx, int l, int r, int v) {
-		if (r < lx || rx < l) {
+	void _update(int x, int lx, int rx, int id, T v) {
+		if (id < lx || rx < id) {
 			return;
 		}
-		if (l <= lx && rx <= r) {
+
+		if (lx == rx) {
             tree[x] = v;
 			return;
 		}
+
 		int mx = (lx + rx) >> 1;
-		_update(ls(x), lx, mx, l, r, v);
-		_update(rs(x), mx + 1, rx, l, r, v);
-		if constexpr (upd_pol == MIN) {
-            tree[x] = min(tree[ls(x)], tree[rs(x)]);
-        } else if constexpr (upd_pol == MAX) {
-            tree[x] = max(tree[ls(x)], tree[rs(x)]);
-		} else {
-            tree[x] = sum(tree[ls(x)], tree[rs(x)]);
-		}
+
+		_update(ls(x), lx, mx, id, v);
+		_update(rs(x), mx + 1, rx, id, v);
+
+		tree[x] = T::merge(tree[ls(x)], tree[rs(x)]);
 	}
 
-    void update(int l, int v) {
-		_update(0, 0, n - 1, l, l, v);
+    void update(int id, T v) {
+		_update(0, 0, n - 1, id, v);
 	}
 };
  
 void solve()
 {
-	int n; cin >> n;
-	vt<int> a(n); read(a);
-	seg_tree<UPD_POL::MIN> tree = seg_tree<UPD_POL::MIN>(a);
-	tree.build();
+	int n, m; cin >> n >> m;
+	vt<node> a(n); 
+	For(i, n) {
+		cin >> a[i].value;
+	}
+	seg_tree_single<node> tree = seg_tree_single<node>(a);
+	For(i, m) {
+		int op; cin >> op;
+		if (op == 1) {
+			int id, v; cin >> id >> v;
+			tree.update(id, node(v));
+		} else {
+			int l, r; cin >> l >> r; l--, r--;
+			node n = tree.get(l, r);
+			cout << n.value << endl;
+		}
+	}
 }
- 
+
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
