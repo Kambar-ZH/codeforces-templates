30049b28665f99b2d3ae2b949d0d8c0d1a64e36c
Kambar-ZH <zhamauov02@mail.ru>
2024-01-02 23:24:01 +0500

    added 01 knapsack

---
+++ templates/dp/01knapsack.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define For(i, n)           for(int i = 0; i < n; ++i)
+#define all(x)              (x).begin(),(x).end()
+#define rall(x)             (x).rbegin(),(x).rend()
+#define ls(x)               x+x+1
+#define rs(x)               x+x+2
+#define endl                '\n'
+
+#define ld                  long double
+#define pii                 pair<int, int>
+#define vt                  vector
+#define ll                  long long
+
+template<typename T>
+void read(vt<T> & a) {For(i, a.size()) cin >> a[i];}
+template<typename T>
+void read2(vt<vt<T> > & a) {For(i, a.size()) read(a[i]);}
+template<typename T>
+void print(vt<T> & a) {For(i, a.size()) cout << a[i] << " "; cout << endl;}
+template<typename T>
+void print2(vt<vt<T> > & a) {For(i, a.size()) print(a[i]);}
+
+const int MAX = 1e9;
+const int MOD = 1e9+7;
+const ll  INF = 1e18;
+const ld  PI  = 3.14159265358979323846;
+
+const int W = 2e5 + 1;
+bitset<W> b;
+
+void solve() {
+    int n, w; cin >> n >> w;
+    b[0] = 1;
+    For(i, n) {
+        int x; cin >> x;
+        b |= (b << x);
+    }
+    assert(w < W);
+    for (int i = w; i >= 0; i--) {
+        if (b[i]) {
+            cout << i << endl;
+            break;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
